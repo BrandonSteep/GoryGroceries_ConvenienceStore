159c60a89019355dc3b9c6eebf94d3f312434d7c
Brandon Steep <jacobwetherall@hotmail.co.uk>
2024-04-16 11:03:56 +0100

    Added functionality to properly remove shopping list items as expected - needs further testing, though initial tests seem promising

--- Assets/Scripts/3_Shopping/Other Items/OtherItemTracker.cs
+++ Assets/Scripts/3_Shopping/Other Items/OtherItemTracker.cs
@@ -23,30 +23,51 @@ public class OtherItemTracker : MonoBehaviour
     }
 
     public void AddAllHeldItems(){
-        // Create a List of ItemSOs from the ShoppingList ((This should really be moved to the ShoppingListInventory Script))
+        // Create a List of ItemSOs from the ShoppingList
+        // THIS should really be moved to the ShoppingListInventory Script
         var shoppingList = GameObject.FindWithTag("Shopping List").GetComponent<ShoppingListInventory>().CheckShoppingList();
         List<ItemSO> shoppingListItemSOs = new List<ItemSO>();
             for(int i = 0; i < shoppingList.Count; i++){
                 shoppingListItemSOs.Add(shoppingList[i].itemSO);
             }
 
-        // Create Inventory List and Remove ShoppingListItems
+        // Create 'inventory' List from PlayerInventory and Remove all items from shoppingListItemSO
         List<ItemSO> inventory = ControllerReferences.playerInventory.CheckInventory().Except(shoppingListItemSOs).ToList();
+        // Create 'otherItemsOnShoppingList' List from PlayerInventory and Remove all items NOT found in shoppingListItemSO
+        List<ItemSO> otherItemsOnShoppingList = ControllerReferences.playerInventory.CheckInventory();
+        otherItemsOnShoppingList.RemoveAll( itemSO => !shoppingListItemSOs.Contains(itemSO));
 
-            for(int i = inventory.Count-1; i > -1; i--){
-                if(inventory[i] == null){
-                    // Clear out empty slots
-                    Debug.Log($"Inventory Item #{i+1} Removed");
-                    inventory.Remove(inventory[i]);
+        // Loop through the 'otherItemsOnShoppingList' List to 
+        for(int i = otherItemsOnShoppingList.Count-1; i > -1; i--){
+
+            // Get ShoppingItem with the same ItemSO
+            var shoppingItem = shoppingList.First(ShoppingItem => ShoppingItem.itemSO == otherItemsOnShoppingList[i]);
+            // Check whether the Item is NOT PAID
+            if(shoppingItem.status != ShoppingItemStatus.Paid){
+                // IF NOT, Remove ONE version of that item from 'otherItemsOnShoppingList' (as one of them will be included in the MISSION total)
+                Debug.Log("Removing Item, as it will be counted as STOLEN in Mission Total");
+                otherItemsOnShoppingList.Remove(shoppingItem.itemSO);
+            }
+            // If one is found, do nothing - all of this item held in the inventory should be considered stolen
+        }
+        // Add the remaining 'otherItemsOnShoppingList' List onto the 'inventory' List, ensuring that all items that should be considered stolen are 
+        inventory.AddRange(otherItemsOnShoppingList);
+
+        for(int i = inventory.Count-1; i > -1; i--){
+            if(inventory[i] == null){
+                // Clear out empty slots
+                Debug.Log($"Inventory Item #{i+1} Removed");
+                inventory.Remove(inventory[i]);
+                }
+            else{
+                // If the item is not Allowed, add to otherItems List as Unpaid
+                if(otherItems.Any(OtherItem => OtherItem.itemSO == inventory[i])){
+                    Debug.Log($"Item {inventory[i].name} is Allowed");
+                    continue;
                 }
                 else{
-                    // If the item is not Allowed, add to otherItems List as Unpaid
-                    if(otherItems.Any(OtherItem => OtherItem.itemSO == inventory[i])){
-                        continue;
-                    }
-                    else{
-                        Debug.Log($"Adding {inventory[i].name} to the Other Items List as UNPAID");
-                        otherItems.Add(new OtherItem(inventory[i], OtherItemStatus.Unpaid));
+                    Debug.Log($"Adding {inventory[i].name} to the Other Items List as UNPAID");
+                    otherItems.Add(new OtherItem(inventory[i], OtherItemStatus.Unpaid));
                     }
                 }
             }
